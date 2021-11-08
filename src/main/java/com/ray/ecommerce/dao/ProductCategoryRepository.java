e63992d03dea5059e0b95b8b9e63bc3b1237d30b
lekyvong09 <lekyvong09@gmail.com>
2021-11-08 20:00:54 +0700

    Repository & "Controller" Product & ProductCategory

---
+++ src/main/java/com/ray/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ray.ecommerce.dao;
+
+import com.ray.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
