21379c145e8d487ff98cb3d0768c2c09bf62cc7f
lekyvong09 <lekyvong09@gmail.com>
2021-11-10 20:29:56 +0700

    eCommerce - find book by categoryId

--- src/main/java/com/ray/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ray/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.ray.ecommerce.dao;
 
 import com.ray.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // select * from product where category_id = ?
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
