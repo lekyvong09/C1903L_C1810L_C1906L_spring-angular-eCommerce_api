5fce6ba40b551f164b5cc087cca09fcdf562782b
lekyvong09 <lekyvong09@gmail.com>
2021-11-10 20:47:49 +0700

    eCommerce - expose Id of the Entity Class

--- src/main/java/com/ray/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/ray/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,28 @@ package com.ray.ecommerce.config;
 
 import com.ray.ecommerce.entity.Product;
 import com.ray.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -23,5 +37,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         config.getExposureConfiguration().forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
+
+        config.exposeIdsFor(arrayOfEntities());
     }
+
+    private Class<?>[] arrayOfEntities() {
+
+        // get a list of all entity classes from metadata
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // get the entity type
+        for (EntityType tempEntityType: entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // convert to array
+        return entityClasses.toArray(new Class[0]);
+    }
+
+
 }
