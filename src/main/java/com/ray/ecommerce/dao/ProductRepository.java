3fbfe503b2176decb750692d76973b6dd1d73e6e
lekyvong09 <lekyvong09@gmail.com>
2021-11-12 20:13:52 +0700

    eCommerce - find product by productName

--- src/main/java/com/ray/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ray/ecommerce/dao/ProductRepository.java
@@ -4,6 +4,7 @@ import com.ray.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,4 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // select * from product where category_id = ?
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    //@Query(value = "select p from Product p where p.name like concat('%',:name,'%')") //JPQL
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
