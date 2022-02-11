983e1de0c14449e8b9e7af7bcaf0e91124d95100
lekyvong09 <lekyvong09@gmail.com>
2022-02-11 21:22:34 +0700

    Database - Replace Customer with User 3

--- src/main/java/com/ray/ecommerce/dao/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ray.ecommerce.dao;
-
-import com.ray.ecommerce.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
